0e3c6a50bea62bac65e6bfb4947eac7ed6b6e540
0xF11 <140972657+0xF11@users.noreply.github.com>
2025-05-28 21:13:39 -0700

    Create loggy.cpp

---
+++ loggy.cpp
@@ -0,0 +1,19 @@
+#pragma once
+#include <iostream>
+
+
+
+void textel(std::string texter)
+{
+	std::cout << texter << std::endl;
+}
+void text(std::string texterr)
+{
+	std::cout << texterr;
+}
+/*
+void input(std::string inp)
+{
+	std::cin >> inp;
+}
+*/
